3da7d0a2c669b56fee9d2557406db7618f10cf52
arianaazari <aazari@bu.edu>
2025-04-02 15:45:21 -0400

    Edited my HTML and CSS

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,5 +6,12 @@
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
 
+// Test to make sure my JS was connected to my HTML
+
 console.log("Hello from Ariana!");
+
+// Used ChatGPT to help me create a clickable button in HTML and JS
+
+document.getElementById("myButton").addEventListener("click", function () {
+});
 
